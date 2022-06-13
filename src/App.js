6450f27802a3a8b5ca1200ac4c9207ab298e6f60
JuanSebastinGB <3393@holbertonschool.com>
2022-06-12 20:20:26 -0500

    Testing single change

--- src/App.js
+++ src/App.js
@@ -12,23 +12,22 @@ function App() {
   const { results } = fetchedData;
   const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
   useEffect(() => {
-    // eslint-disable-next-line func-names
     (async function () {
-      const data = await fetch(api).then((res) => res.json());
+      const data = await fetch(api).then(res => res.json());
       updateFetchedData(data);
     })();
   }, [api]);
   return (
-    <div className="App">
-      <h1 className="text-center ubuntu my-4">
-        Rick & Morty <span className="text-primary">Wiki</span>
+    <div className='App'>
+      <h1 className='text-center ubuntu my-4'>
+        Rick & Morty <span className='text-primary'>Wiki</span>
       </h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-3">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-3'>
             <Filters />
           </div>
-          <div className="col-8">
+          <div className='col-8'>
             <Cards results={results} columnsQuantity={3} />
           </div>
         </div>
